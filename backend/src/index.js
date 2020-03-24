8cdfdb4933906dcfe547f5562f4b9d2732903a7a
isterra <isterra00@gmail.com>
2020-03-24 13:33:26 -0300

    Back-end da aplicacao

---
+++ backend/src/index.js
@@ -0,0 +1,9 @@
+///home/isterra/Documentos/SemanaOmnistack-11/aulas/backend/src/database/migrations
+const express = require('express');
+const routes = require('./routes')
+const cors = require('cors')
+const app= express();
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.listen(3333);
